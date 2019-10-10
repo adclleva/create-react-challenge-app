131d37e058b5bdaea7e6138a282ef539149ee7a2
ArvinLleva <arvin.lleva@yahoo.com>
2019-10-09 18:48:08 -0700

    first commit of this challenge

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Arvin Lleva</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
